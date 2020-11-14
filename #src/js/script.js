707d3fd25c169258fc79742cb87b1f611f9e4430
frontEndDevel <front_end_dev@yahoo.com>
2020-11-14 11:05:52 +0200

    home section desktop done

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,7 +1,5 @@
 @@include("libs/slick.min.js");
 
 (function () {
-  @@include("scripts/components/_menu.js");
-  @@include("scripts/components/_progressbar.js");
-  @@include("scripts/components/_slider.js");
+	@@include("scripts/components/_header.js");
 })();
